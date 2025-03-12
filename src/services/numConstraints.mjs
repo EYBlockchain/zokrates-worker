ab8d96440e62a5b380e958c9080543980f3b4d48
Lydia Garms <Lydia.Garms@uk.ey.com>
2025-03-12 15:56:30 +0000

    number of constraints endpoint

--- src/services/numConstraints.mjs
+++ src/services/numConstraints.mjs
@@ -3,7 +3,7 @@ import path from 'path';
 import { compile, extractVk, exportKeys, setup } from '../zokrates-lib/index.mjs';
 import logger from '../utils/logger.mjs';
 
-export default async function generateKeys({ filepath, curve = 'bn128' }) {
+export default async function numConstraints({ filepath, curve = 'bn128' }) {
   const outputPath = `./output`;
   const circuitsPath = `./circuits`;
 
@@ -27,19 +27,21 @@ export default async function generateKeys({ filepath, curve = 'bn128' }) {
     `${circuitName}_out`,
     curve,
   );
+  const regex = /Number of constraints:\s*(\d+)/;
+  console.log(compileResult);
+  const match = compileResult.match(regex);
+  let numberOfConstraints;
+  if (match) {
+    numberOfConstraints = match[1];
+    const limit = 20000;
+    if (numberOfConstraints > limit) {
+      throw new Error(`The circuit has ${numberOfConstraints} constraints, which exceeds the limit of ${limit} constraints from the current subscription plan.`);
+    } else {
+      console.log(`Number of constraints in the circuit does not exceed limit of ${limit}:`, numberOfConstraints);
+    }
+  } else {
+    throw new Error('Number of constraints not found, due to compilation failure.');
+  }
 
-    logger.info('Setup...');
-    await setup(
-      `${outputPath}/${circuitDir}/${circuitName}_out`,
-      `${outputPath}/${circuitDir}`,
-      'g16',
-      'bellman',
-      `${circuitName}_vk`,
-      `${circuitName}_pk`,
-    );
-
-  const vk = await extractVk(`${outputPath}/${circuitDir}/${circuitName}_vk.key`);
-
-  logger.info(`Complete ${filepath}`);
-  return { vk, filepath };
+  return {  numberOfConstraints };
 }
